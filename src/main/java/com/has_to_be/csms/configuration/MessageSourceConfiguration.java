f4502987cee0f5403f2ce6ff3ef45c577738081b
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-05 20:03:38 +0330

    MessageResourceUtility.java added 1- a utility class to read from    MessageSource

---
+++ src/main/java/com/has_to_be/csms/configuration/MessageSourceConfiguration.java
@@ -0,0 +1,22 @@
+package com.has_to_be.csms.configuration;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class MessageSourceConfiguration {
+    @Bean
+    @Qualifier("validationMessageSource")
+    public MessageSource validationMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/validation");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        return messageSource;
+    }
+}
