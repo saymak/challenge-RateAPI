236fce6c9bcb551716b471259294155efd35676d
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-06 01:35:22 +0330

    ApplyRateCommandDTO.java updated 1- new class and field level comments added

--- src/main/java/com/has_to_be/csms/dto/RateQueryDTO.java
+++ src/main/java/com/has_to_be/csms/dto/RateQueryDTO.java
@@ -1,18 +1,29 @@
 package com.has_to_be.csms.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 import java.math.BigDecimal;
 
-
+/**
+ * Returned as response body dto of /rate API
+ */
 @Value
 public class RateQueryDTO {
 
+    /**
+     * overall cost of charging process rounded 2 decimal places
+     */
     @JsonProperty("overall")
+    @Schema(example = "7.04", description = "overall cost of charging process rounded 2 decimal places")
     private BigDecimal overall;
 
+    /**
+     * rate components serialized as "components" object include calculated cost details of charging process
+     */
     @JsonProperty("components")
     private RateComponentDTO rateComponentDTO;
 
 }
+
