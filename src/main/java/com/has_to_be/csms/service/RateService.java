2503206dcb1ee208437e8a73aa1ed4d97a14e7ba
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-05 21:52:07 +0330

    RateService.java updated 1- comments added on RateService interface 2- comments added on rate() method

--- src/main/java/com/has_to_be/csms/service/RateService.java
+++ src/main/java/com/has_to_be/csms/service/RateService.java
@@ -4,8 +4,17 @@ import com.has_to_be.csms.dto.ApplyRateCommandDTO;
 import com.has_to_be.csms.dto.RateQueryDTO;
 
 
+/**
+ * The interface Rate service.
+ */
 public interface RateService {
 
+    /**
+     * Apply rate rate query dto.
+     *
+     * @param applyRateCommandDTO dto of a rate request details
+     * @return RateQueryDTO dto of a rete calculation result result
+     */
     RateQueryDTO applyRate(ApplyRateCommandDTO applyRateCommandDTO);
 
 }
