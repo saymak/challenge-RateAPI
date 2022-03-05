1049f54579d37dccbd80362a67b62226bce951ea
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-05 22:05:39 +0330

    BusinessException.java added 1- parent class all BusinessException classes 2- getMessageKey() calls the getCanonicalName()   of each business exception class. result of   getMessageKey() is used for finding corresponding   exception message of thrown exception

---
+++ src/main/java/com/has_to_be/csms/exception/BusinessException.java
@@ -0,0 +1,25 @@
+package com.has_to_be.csms.exception;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class BusinessException extends RuntimeException {
+    /**
+     * static message keys
+     */
+    public static final String DEFAULT_BAD_REQUEST_MESSAGE_KEY = "backend.exception.data.format.is.invalid";
+
+    public static final String DEFAULT_INTERNAL_SERVER_ERROR_KEY = "backend.exception.static.internal.server.error";
+
+    protected BusinessException() {
+    }
+
+    protected BusinessException(Exception e) {
+        super(e);
+    }
+
+    public abstract HttpStatus getStatusCode();
+
+    public final String getMessageKey() {
+        return this.getClass().getCanonicalName();
+    }
+}
