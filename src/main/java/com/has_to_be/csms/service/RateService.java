53319f8c00b81042bcd45a0211c0965068574ac9
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-06 13:19:34 +0330

    RateService.java updated 1- redundant comment removed 2- comments enhanced

--- src/main/java/com/has_to_be/csms/service/RateService.java
+++ src/main/java/com/has_to_be/csms/service/RateService.java
@@ -3,17 +3,13 @@ package com.has_to_be.csms.service;
 import com.has_to_be.csms.dto.ApplyRateCommandDTO;
 import com.has_to_be.csms.dto.RateQueryDTO;
 
-
-/**
- * The interface Rate service.
- */
 public interface RateService {
 
     /**
-     * Apply rate rate query dto.
+     * Apply supplied rates on rating request.
      *
      * @param applyRateCommandDTO dto of a rate request details
-     * @return RateQueryDTO dto of a rete calculation result result
+     * @return {@link RateQueryDTO}
      */
     RateQueryDTO applyRate(ApplyRateCommandDTO applyRateCommandDTO);
 
