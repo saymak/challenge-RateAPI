639d3caf3152b7239476986069dcce6faca47bc2
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-06 05:05:08 +0330

    CdrDTO.java updated 1- java doc added 2- swagger schema annotation added above    property

--- src/main/java/com/has_to_be/csms/dto/CdrDTO.java
+++ src/main/java/com/has_to_be/csms/dto/CdrDTO.java
@@ -1,29 +1,48 @@
 package com.has_to_be.csms.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-
+/**
+ * The {@link CdrDTO} class that includes CDR (Charging Data Record) details.
+ */
 @Value
 public class CdrDTO {
 
+    /**
+     * meter value of the electricity meter when the charging process started
+     */
     @NotNull(message = "NotNull.meterStart.CdrDTO")
     @Min(value = 0, message = "Min.meterStart.CdrDTO")
+    @Schema(example = "1204307", description = "electricity meter start value")
     private Long meterStart;
 
+    /**
+     * meter value of the electricity meter when the charging process stopped
+     */
     @NotNull(message = "NotNull.meterStop.CdrDTO")
     @Min(value = 0, message = "Min.meterStop.CdrDTO")
+    @Schema(example = "1215230", description = "electricity meter stop value")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Long meterStop;
 
+    /**
+     * timestamp (according to ISO 8601) when the charging process started
+     */
     @NotNull(message = "NotNull.timestampStart.CdrDTO")
+    @Schema(example = "2021-04-05T10:04:00Z", description = "process start in timestamp")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private String timestampStart;
 
+    /**
+     * timestamp (according to ISO 8601) when the charging process stopped
+     */
     @NotNull(message = "NotNull.timestampStop.CdrDTO")
+    @Schema(example = "2021-04-05T11:27:00Z", description = "process stop in timestamp")
     private String timestampStop;
 
 }
