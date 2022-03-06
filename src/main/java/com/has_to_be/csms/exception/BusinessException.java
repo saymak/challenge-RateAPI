a4ed1ca2e30e393b6b3619b3995f8d024bf1a26e
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-06 05:37:30 +0330

    BusinessException.java updated 1- redundant java doc and removed 2- a new line added

--- src/main/java/com/has_to_be/csms/exception/BusinessException.java
+++ src/main/java/com/has_to_be/csms/exception/BusinessException.java
@@ -3,9 +3,7 @@ package com.has_to_be.csms.exception;
 import org.springframework.http.HttpStatus;
 
 public abstract class BusinessException extends RuntimeException {
-    /**
-     * static message keys
-     */
+
     public static final String DEFAULT_BAD_REQUEST_MESSAGE_KEY = "backend.exception.data.format.is.invalid";
 
     public static final String DEFAULT_INTERNAL_SERVER_ERROR_KEY = "backend.exception.static.internal.server.error";
@@ -22,4 +20,5 @@ public abstract class BusinessException extends RuntimeException {
     public final String getMessageKey() {
         return this.getClass().getCanonicalName();
     }
+
 }
