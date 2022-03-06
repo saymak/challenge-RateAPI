153ea8836969debe9265069a8847f183ee9a91f5
Saeed Alizadeh <jsaeed_alizadeh@yahoo.com>
2022-03-06 16:22:55 +0330

    RateRestControllerTest.java updated 1- import org.springframework.http.HttpStatus    was missing

--- src/test/java/com/has_to_be/csms/controller/RateRestControllerTest.java
+++ src/test/java/com/has_to_be/csms/controller/RateRestControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
